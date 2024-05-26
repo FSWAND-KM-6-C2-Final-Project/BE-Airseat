a61083aa089cd1193c7c3326ae552f79aebda020
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-05-26 23:19:26 +0700

    Requiring flightRouter

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,13 @@ const swaggerDocument = require("../docs/swagger.json");
     Import other routers here, for example:
     const authRouter = require("./authRouter");
 */
+const flightRouter = require("./flightRouter");
 
 /*
     Define other routes here, for example:
     router.use("/api/v1/auth", authRouter);
 */
 
-router.use("/api/v1/flight", flightRouter)
+router.use("/api/v1/flight", flightRouter);
 
 module.exports = router;
