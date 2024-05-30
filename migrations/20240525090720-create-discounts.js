f9af3bdb3fd97876e37d93bd01bce984e87505d6
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-05-30 17:35:27 +0700

    Adjust TableName

--- migrations/20240525090720-create-discounts.js
+++ migrations/20240525090720-create-discounts.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("discounts", {
+    await queryInterface.createTable("Discounts", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("discounts");
+    await queryInterface.dropTable("Discounts");
   },
 };
