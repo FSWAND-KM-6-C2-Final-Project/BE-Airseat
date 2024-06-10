7cd46cd154794b8b8b5fb5da5e229ea5273721fc
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-06-10 18:59:21 +0700

    Done Dashboard for airline

--- routes/adminAirlineRouter.js
+++ routes/adminAirlineRouter.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
 
+const upload = require("../utils/multerConfig");
+
 const adminAirlineController = require("../controllers/adminAirlineController");
 
 router.route("/list").get(adminAirlineController.listAirline);
+router
+  .route("/add")
+  .get(adminAirlineController.createAirlinePage)
+  .post(
+    upload.single("airline_picture"),
+    adminAirlineController.insertAirlines
+  );
+
+router.route("/delete/:id").post(adminAirlineController.deleteAirline);
+
+router.route("/edit/:id").get(adminAirlineController.editAirlinePage);
+
+router
+  .route("/update/:id")
+  .post(upload.single("airline_picture"), adminAirlineController.updateAirline);
 
 module.exports = router;
