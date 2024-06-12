e066b93f16ec28c0762b721541af0f70c2199595
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-06-12 20:52:05 +0700

    Fix setup lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  {
+    ignores: ["public/*"],
+  },
+  { languageOptions: { globals: globals.browser } },
 ];
